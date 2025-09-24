38d18a91a40f19f983941f519a80b7e27bdac5fa
ProgramadorErrejota <pretodesigngrafico@gmail.com>
2025-09-23 21:56:00 -0300

    Desafio: Nível Aventureiro

--- super_trunfo.c
+++ super_trunfo.c
@@ -2,19 +2,22 @@
 
 int main(){
     char estado1, estado2;
-    char codigo1[4], codigo2[4]; // Foi colocado um char a mais para buffer
+    char codigo1[4], codigo2[4]; // colocar um char a mais para buffer
     char nome1[60], nome2[60]; 
     int pop1, pop2;
     float area1, area2;
     float pib1, pib2;
     int tur1, tur2;
 
+    float densidade_pop1, densidade_pop2;
+    float pib_per_capita1, pib_per_capita2;
+
         // Le as informações da primeira carta
     printf("Insira as informações da primeira carta:\n");
     //Le o estado
     printf("Estado: ");
     scanf(" %c", &estado1);
-    fflush(stdout); // limpa o cache para evitar problema com scanf
+    fflush(stdout);
 
     //Le o codigo
     printf("Codigo: ");
@@ -23,7 +26,7 @@ int main(){
 
     //Le o nome
     printf("Nome: ");
-    scanf(" %[^\n]", nome1); // %[ˆ\n] pois não estava lendo direito no Mac
+    scanf(" %[^\n]", nome1);
     fflush(stdout);
 
     //Le a populacao
@@ -70,14 +73,24 @@ int main(){
     scanf("%d", &tur2);
     fflush(stdout);
 
+    //Calculo da densidade populacional e pib per capita da carta 1
+    densidade_pop1 = pop1/area1;
+    pib_per_capita1 = pib1 * 1000000000/ pop1;
+
+    //Calculo da densidade populacional e pib per capita da carta 2
+    densidade_pop2 = pop2/area2;
+    pib_per_capita2 = pib2 * 1000000000 / pop2;
+
 
     //Imprime as informações da primeira carta
-    printf("\nCarta 1:\nEstado: %c\nCódigo: %s\nNome da Cidade: %s\nPopulação: %d\nÁrea: %.2f km2\nPIB: %.2f bilhões de reais\nNúmero de Pontos Turísticos: %d\n", estado1, codigo1, nome1, pop1, area1, pib1, tur1);
+    printf("\nCarta 1:\nEstado: %c\nCódigo: %s\nNome da Cidade: %s\nPopulação: %d\nÁrea: %.2f km2\nPIB: %.2f bilhões de reais\nNúmero de Pontos Turísticos: %d\nDensidade Populacional: %.2f hab/km2\nPIB per Capita: %.2f reais\n", estado1, codigo1, nome1, pop1, area1, pib1, tur1, densidade_pop1, pib_per_capita1);
     fflush(stdout);
 
     //Imprime as informações da segunda carta
-    printf("\nCarta 2:\nEstado: %c\nCódigo: %s\nNome da Cidade: %s\nPopulação: %d\nÁrea: %.2f km2\nPIB: %.2f bilhões de reais\nNúmero de Pontos Turísticos: %d\n", estado2, codigo2, nome2, pop2, area2, pib2, tur2);
+    printf("\nCarta 2:\nEstado: %c\nCódigo: %s\nNome da Cidade: %s\nPopulação: %d\nÁrea: %.2f km2\nPIB: %.2f bilhões de reais\nNúmero de Pontos Turísticos: %d\nDensidade Populacional: %.2f hab/km2\nPIB per Capita: %.2f reais\n", estado2, codigo2, nome2, pop2, area2, pib2, tur2, densidade_pop2, pib_per_capita2);
     fflush(stdout);
+
+
 
     return 0;
 }
